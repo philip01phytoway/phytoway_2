ce21cdf66e04a89e38f38d74c16f84ad52ce42fd
gubon01 <guswnd8819@gmail.com>
2023-05-16 18:17:29 +0900

    2023-05-16 퇴근 커밋

--- 쿼리/쿼리문/데이터현황파악.sql
+++ 쿼리/쿼리문/데이터현황파악.sql
@@ -12,6 +12,13 @@ GROUP BY yymm, yyww, order_date
 Order BY order_date DESC
 LIMIT 1000
 
+-- 전주 동요일 대비
+
+-- 최근 8주간 동요일 대비
+
+
+
+-- 이런 분석조차도 미리 준비된 것들을 실행시키기만 하면 되게끔 만들어야 한다.
 
 -- 월별 sum.
 -- 월 목표도 있으니까.
@@ -23,15 +30,100 @@ GROUP BY yymm, yyww, order_date, brand
 Order BY order_date DESC, price desc
 LIMIT 1000
 
+-- 기타 제품이 1000만원 넘게 팔렸으므로 특이한 일이니까 확인해보면
+-- 로켓배송에서 매출이 나왔다.
+SELECT *
+FROM "order_batch"
+WHERE order_date = '2023-05-15' AND nick = '기타'
+Order BY prd_amount_mod DESC
+
+-- 기타 제품을 제외한 매출을 보면
+SELECT yymm, yyww, order_date, SUM(prd_amount_mod) AS price
+FROM "order_batch"
+WHERE phytoway = 'y'
+GROUP BY yymm, yyww, order_date
+Order BY order_date DESC
+LIMIT 1000
+-- interval 30 days로 지난달과 비교해볼 수 있겠다.
+
 
 -- 일자, 상품별
+SELECT yymm, yyww, order_date, brand, nick, SUM(prd_amount_mod) AS price
+FROM "order_batch"
+GROUP BY yymm, yyww, order_date, brand, nick
+Order BY order_date DESC, price desc
+LIMIT 1000
+
+-- 5월 13일에 brand = null인게 있네.
+-- 쿠팡 제트배송 이었네.
+SELECT *
+FROM "order_batch"
+WHERE brand IS NULL
+
+SELECT *
+FROM "coupang_sales"
+WHERE reg_date = '2023-05-13'
+
 
 -- 일자, 스토어별
+SELECT yymm, yyww, order_date, store, SUM(prd_amount_mod) AS price
+FROM "order_batch"
+GROUP BY yymm, yyww, order_date, store
+Order BY order_date DESC, price desc
+LIMIT 1000
+
+
+-- 평균과 변동.
+-- 30일 이동평균 매출도 보고,
+-- 변동도 구해보고,
+
+-- 매출의 분포도 봐야겠다. 정규분포를 띄고 있을까?
+-- 어쩌면 내가 통계분석을 수행하면 데이터에 대한 이해도가 높아질 수 있다.
+
+-- CAGR로 연복리 성장율도 구해보고.
+
+-- 기초적인 통계분석을 진행해보자.
+-- 데이터를 혼자 가지고 놀아보자.
+
 
 -- 신규고객수, 신규매출, 재구매고객수, 재구매매출
 
+-- 명, 건, 개, 액
+
+
+-- 매핑 누락, 중복 확인
+
+-- 데이터 누락, 중복 확인
 
 
 
 -- 네이버 광고
+
+
+-- 매출 분석을 다각도로 시행한 논문이나 책이 있을 것이다.
+-- 그런 책들을 여러권 조사해보면, 내가 매출을 어떻게 파악, 분석해야 하는지 감이 잡힐 것이다.
+-- 아, 그리고 마소캠퍼스에서도 몇가지 기법이 나오지.
+
+
+-- 시중의 여러가지 대시보드 툴이 있을 것이고
+-- 그 툴들이 공통적으로 사용하는 지표들을 조사해서 추릴 수 있을 거다.
+
+-- WBR마스터 시트도 매출분석이 상세하게 되어있는 표이다.
+
+-- 비율도 보고 차이도 본다.
+
+-- 매출에는 시간에 따른 변화가 꼭 물려있구나. 매출은 일자가 있을 수 밖에 없으니까.
+
+
+
+
+
+
+
+-- EZ_Order에 스토어 매핑 누락 확인
+SELECT DISTINCT shop_name
+FROM	"EZ_Order" as o
+LEFT JOIN "store" AS s ON (o.shop_id = s.ez_store_code)
+WHERE s.ez_store_code IS NULL
+
 
--- 쿼리/쿼리문/운영.sql
+++ 쿼리/쿼리문/운영.sql
@@ -440,13 +440,203 @@ WHERE trans_date = '2023-05-05' AND store = '쿠팡' AND nick = '판토모나하
 
 
 
-SELECT SUM(out_qty)
+SELECT store, out_qty --SUM(out_qty)
 FROM 	"order_batch"
-WHERE trans_date = '2023-05-08' AND store = '쿠팡' AND nick = '판토모나하이퍼포머' AND order_status <> '취소'
+WHERE trans_date = '2023-05-08' AND nick = '판토모나하이퍼포머' AND order_status <> '취소'
+AND store NOT IN ('스마트스토어_풀필먼트', '쿠팡_제트배송')
 
+
+SELECT * FROM "store"
+
+AND store = '쿠팡'
+
+
+SELECT * 
+FROM "EZ_Order"
+WHERE shop_name = '추가발송'
+
+
+
+
+SELECT *
+FROM	"EZ_Order" as o
+--WHERE o."options" LIKE '%온유%'
+
+LEFT JOIN "store" AS s ON (o.shop_id = s.ez_store_code)
+WHERE s.ez_store_code IS NULL
 
 
 
 SELECT SUM(out_qty)
 FROM 	"order_batch"
 WHERE trans_date = '2023-05-09' AND store = '쿠팡' AND nick = '판토모나하이퍼포머' AND order_status <> '취소'
+
+
+
+출고에는
+재고이동, 증정, 판매가 있음.
+
+
+
+-- 추가발송건
+SELECT 	
+			trans_date, onnuri_code, onnuri_name,
+			'코린트' AS "warehouse", 
+			SUM(out_qty) * -1 AS out_qty, '증정' AS "stock_type"
+FROM 	(
+			SELECT	
+
+						left(order_date, 10) AS order_date, o2.recv_name, 
+						
+						o2.shop_id, 
+						
+						o2.brand, o2.nick, o2.product_qty, o2.order_qty, o2.product_qty * o2.order_qty AS out_qty,
+		
+						o2.prd_amount_mod,
+						
+						o2.onnuri_code, o2.onnuri_name, o2.onnuri_type, 
+						left(trans_date_pos, 10) AS trans_date
+
+			FROM (
+						SELECT *, p.qty AS order_qty, b.qty AS product_qty,
+									CASE WHEN o.shop_name = '자사몰' AND p.prd_amount = 0 THEN b.price
+									ELSE p.prd_amount
+									END AS prd_amount_mod,
+									p.order_cs AS order_status
+								
+						FROM	"EZ_Order" as o,																		
+								jsonb_to_recordset(o.order_products) as p(																	
+									name character varying(255),																
+									product_id character varying(255),																
+									qty integer, 
+									prd_amount integer,	
+									prd_supply_price integer,															
+									order_cs integer,																
+									cancel_date character varying(255),																
+									change_date character varying(255)																
+								)
+						LEFT JOIN "bundle" as b on (p.product_id = b.ez_code)																	
+						LEFT JOIN "product" as pp on (b.product_no = pp.no)																		
+						WHERE	order_id NOT IN (SELECT order_num FROM "Non_Order")	
+								 AND pp.term > 0 AND o.order_name <> '' --AND p.order_cs = 0
+					) AS o2
+		) AS o3
+LEFT JOIN "store" AS s ON (o3.shop_id = s.ez_store_code)
+WHERE s.no = 47 AND onnuri_type = '판매분매입' AND recv_name NOT LIKE '%전현빈%'
+AND trans_date <> '' AND trans_date IS NOT NULL
+GROUP BY trans_date, onnuri_code, onnuri_name
+
+
+
+
+SELECT *
+FROM (
+SELECT *
+FROM "EZ_Order" AS o,																		
+					jsonb_to_recordset(o.order_products) AS p(																	
+						name CHARACTER VARYING(255),																
+						product_id CHARACTER VARYING(255),																
+						qty INTEGER, 
+						prd_amount INTEGER,	
+						prd_supply_price INTEGER,															
+						order_cs INTEGER,																
+						cancel_date CHARACTER VARYING(255),																
+						change_date CHARACTER VARYING(255)																
+					)
+			LEFT JOIN "bundle" AS b ON (p.product_id = b.ez_code)																	
+			LEFT JOIN "product" AS pp ON (b.product_no = pp.no)	
+) AS t
+WHERE shop_name = '추가발송'
+AND recv_name NOT LIKE '%전현빈%'
+
+
+
+AND product_name NOT LIKE '%두피%' 
+AND product_name NOT LIKE '%약통%' 
+AND product_name NOT LIKE '%구슬%'
+AND order_id IN (SELECT order_num FROM "Non_Order")
+
+
+
+SELECT *
+FROM "E"
+
+
+SELECT collect_date, shop_id, shop_name, product_name, "options", qty
+FROM "EZ_Order"
+WHERE recv_address LIKE '%온유약국%'
+
+
+
+"options" LIKE '%온유%'
+
+shop_id = 10096
+
+order_id = '15140_9'
+
+
+
+
+SELECT *
+FROM "order_batch"
+WHERE order_date = '2023-05-02' AND nick = '판토모나하이퍼포머'
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 써큐시안 선물세트 분리
+
+SELECT *
+FROM 		"naver_order_product" AS n
+LEFT JOIN "naver_option" AS o ON (n."optionCode" = o."option_code")
+LEFT JOIN "product" AS p ON (o."product_no" = p."no")	
+WHERE 	"productOrderStatus" IN ('PAYED', 'DELIVERING', 'DELIVERED', 'PURCHASE_DECIDED', 'EXCHANGED', 'CANCELED', 'RETURNED')
+AND ("productName" LIKE '%선물%' OR "productOption"	LIKE '%선물%') AND "productName" NOT LIKE '%차가%'
+AND n."orderId" NOT IN (SELECT order_num FROM "Non_Order")
+
+
+
+
+SELECT *
+FROM "EZ_Order"
+WHERE product_name LIKE '%선물%' OR "options" LIKE '%선물%' AND product_name NOT LIKE '%MSM%' AND product_name NOT LIKE '%제중원%'
+
+
+
+SELECT *			
+FROM (
+						SELECT *, p.qty AS order_qty, b.qty AS product_qty,
+									CASE WHEN o.shop_name = '자사몰' AND p.prd_amount = 0 THEN b.price
+									ELSE p.prd_amount
+									END AS prd_amount_mod,
+									p.order_cs AS order_status
+								
+						FROM	"EZ_Order" as o,																		
+								jsonb_to_recordset(o.order_products) as p(																	
+									name character varying(255),																
+									product_id character varying(255),																
+									qty integer, 
+									prd_amount integer,	
+									prd_supply_price integer,															
+									order_cs integer,																
+									cancel_date character varying(255),																
+									change_date character varying(255)																
+								)
+						LEFT JOIN "bundle" as b on (p.product_id = b.ez_code)																	
+						LEFT JOIN "product" as pp on (b.product_no = pp.no)																		
+						WHERE	order_date > '2021-09-01' AND order_date < '2023-12-31' 
+								AND order_id NOT IN (SELECT order_num FROM "Non_Order")	
+								 AND pp.term > 0 AND o.order_name <> '' --AND p.order_cs = 0
+		) AS o3
+LEFT JOIN "store" AS s ON (o3.shop_id = s.ez_store_code)
+WHERE product_name LIKE '%선물%' OR "options" LIKE '%선물%' AND product_name NOT LIKE '%MSM%' AND product_name NOT LIKE '%제중원%'
+
